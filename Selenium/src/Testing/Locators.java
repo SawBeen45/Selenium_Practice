2e3ea72186036a217c7b2679fa2dc76c7d6b1275
SawBeen45 <mesabinkhadka@gmail.com>
2025-09-03 17:51:50 +1000

    This is a locators practice in Selenium.

--- Selenium/src/Testing/Locators.java
+++ Selenium/src/Testing/Locators.java
@@ -1,21 +1,47 @@
 package Testing;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Kree_tika/OneDrive/Desktop/selenium/chromedriver-win64/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("Sabin");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
 		driver.findElement(By.className("signInBtn")).click();
 		
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@abc.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("4525452252");
+		
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		
+		System.out.println(driver.findElement(By.cssSelector(".infoMsg")).getText());
+		
+		driver.findElement(By.cssSelector(".go-to-login-btn")).click();
+		Thread.sleep(1000);
+		
+		//full login with  correct credentials
+		
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("Sabin");
+		driver.findElement(By.cssSelector("input[placeholder='Password']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector("#chkboxOne")).click();
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
 	}
 
 }
